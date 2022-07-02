343b06e0b21a4744e9306c08cb75bbeeff0854a3
HIND <107958492+Hfalo@users.noreply.github.com>
2022-07-02 07:11:29 +0300

    home work 1 which i apparently forgot about :)

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿string hero = "Link";
+int health = 100;
+
+int Damaged_health = health - 20;
+int Boosted_health = health - 10;
+
+Console.WriteLine("Damaged health : " + Damaged_health);
+Console.WriteLine("Boosted health : " + Boosted_health);
+
+Console.WriteLine(Damaged_health + " is less than " + Boosted_health);
